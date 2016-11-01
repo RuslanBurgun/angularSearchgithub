a26b54928ee9b8823020f1fb1f79cf55d805d5fc
Ruslan Burgun <burgun90@mail.ru>
2016-11-02 00:30:25 +0300

    add servcie and components

--- app/components/profile.component.ts
+++ app/components/profile.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
+import { GithubService } from '../services/github.service';
 
 @Component({
     moduleId: module.id,
     selector: 'profile',
-    templateUrl: `profile.component.html`
+    templateUrl: `profile.component.html`,
 })
-export class ProfileComponent { }
+export class ProfileComponent implements OnInit { 
+    
+     user:any[];
+
+   
+    constructor(private _githubServcie: GithubService){
+
+     }
+
+    ngOnInit() : void {
+         this.getUser();
+    }
+
+    getUser() : void{
+        this._githubServcie.getUser().subscribe(user=> {
+        this.user = user;   
+        });
+    }
+         
+         
+   
+}
---
+++ app/services/github.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/toPromise';
+
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GithubService{
+    private username : string;
+    private client_id = '440b4a9043f3237aa686';
+    private client_secret = 'bc3a4ba5d9f3290ee208c7e6d663459106cf4ca0';
+    
+    constructor(private _http: Http){
+        console.log('githubservice ready');
+        this.username = 'RuslanBurgun';
+    }
+
+    getUser(){
+        return this._http.get('http://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret='+this.client_secret)
+            .map(res=>res.json());
+    }
+
+}
+
