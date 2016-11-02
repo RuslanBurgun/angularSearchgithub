4f9b398ed8dd25fdf3aaefd64dd67123a7e4ec26
Ruslan Burgun <burgun90@mail.ru>
2016-11-02 11:23:55 +0300

    add repo method in service

--- app/components/profile.component.ts
+++ app/components/profile.component.ts
@@ -11,6 +11,7 @@ import { GithubService } from '../services/github.service';
 export class ProfileComponent implements OnInit { 
     
      user:any[];
+     repos:any[];
 
    
     constructor(private _githubServcie: GithubService){
@@ -19,14 +20,21 @@ export class ProfileComponent implements OnInit {
 
     ngOnInit() : void {
          this.getUser();
+         this.getRepos();
     }
 
     getUser() : void{
         this._githubServcie.getUser().subscribe(user=> {
         this.user = user;   
         });
-        console.log(this.user)
     }
+
+    getRepos() : void{
+        this._githubServcie.getRepos().subscribe(repos=> {
+        this.repos = repos;  
+        });
+    }
+
          
          
    
